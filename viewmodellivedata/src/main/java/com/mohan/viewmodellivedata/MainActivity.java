976748b72abed1f1189d4d9572a04714fc623da8
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2021-03-25 14:18:33 +0530

    Create Random Numbers: LiveData Demo Implemented
    Reference Link:
    https://www.youtube.com/watch?v=1Tn7TuHUl4Y&list=PLlxmoA0rQ-LyVuVR1LFvpR1K8A0HsIBYx&index=6&ab_channel=Smartherd

--- viewmodellivedata/src/main/java/com/mohan/viewmodellivedata/MainActivity.java
+++ viewmodellivedata/src/main/java/com/mohan/viewmodellivedata/MainActivity.java
@@ -2,9 +2,13 @@ package com.mohan.viewmodellivedata;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView mTVrandom = findViewById(R.id.tV_Number);
+        Button mBtnRandom =findViewById(R.id.button);
         //MainActivityViewModel mDataGenrator = new MainActivityViewModel();
         MainActivityViewModel mModelObj= new ViewModelProvider(this).get(MainActivityViewModel.class);
-        String mRandomNumber = mModelObj.getRandomNumber();
-        mTVrandom.setText(mRandomNumber);
+        LiveData<String> mRandomNumber = mModelObj.getRandomNumber();
+        mRandomNumber.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+
+                mTVrandom.setText(s);
+            }
+        });
+        mBtnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mModelObj.createNumber();
+            }
+        });
         Log.i(TAG,"Random Number Set");
     }
 }
--- viewmodellivedata/src/main/java/com/mohan/viewmodellivedata/MainActivityViewModel.java
+++ viewmodellivedata/src/main/java/com/mohan/viewmodellivedata/MainActivityViewModel.java
@@ -2,26 +2,29 @@ package com.mohan.viewmodellivedata;
 
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.Random;
 
 public class MainActivityViewModel extends ViewModel {
     private String TAG =this.getClass().getSimpleName();
-    private String RandomNumber;
+    private MutableLiveData<String> mRandomNumber;
 
-    public String getRandomNumber(){
+    public MutableLiveData<String> getRandomNumber(){
         Log.i(TAG,"Get Number");
-        if(RandomNumber==null){
+        if(mRandomNumber==null){
+            mRandomNumber= new MutableLiveData<String>();
             createNumber();
         }
-        return RandomNumber;
+        return mRandomNumber;
     }
 
-    private void createNumber() {
+    public void createNumber() {
         Log.i(TAG,"Create New number");
         Random mRandom=new Random();
-        RandomNumber = "Number:" + (mRandom.nextInt(100-1)+1);
+        //mRandomNumber = "Number:" + (mRandom.nextInt(100-1)+1);
+        mRandomNumber.setValue("Number:" + (mRandom.nextInt(100-1)+1));
     }
 
     @Override
