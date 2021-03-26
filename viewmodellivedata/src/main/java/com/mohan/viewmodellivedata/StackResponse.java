4ceaf43ef278c532f4bcc230d7499818f9f3be75
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2021-03-26 13:45:26 +0530

    PagingListAdapter Done: 1-Implemented PagingListAdapter 2-Added RecyclerView DiffUtils In-Progress: 1-Show values in recyclerview 2-add Pagination in recyclerview
    Reference Api:
    https://api.stackexchange.com/docs/answers#page=2&pagesize=50&order=desc&sort=activity&filter=default&site=stackoverflow&run=true
    
    API Video Reference  Link:
    https://www.youtube.com/watch?v=xemIEMLTeTM&list=PLk7v1Z2rk4hjCQw1RVoYPRdeIzwdz5_Fi&index=3&ab_channel=SimplifiedCoding
    
    Pagging Reference Link:
    https://www.youtube.com/watch?v=xemIEMLTeTM&list=PLk7v1Z2rk4hjCQw1RVoYPRdeIzwdz5_Fi&index=4&ab_channel=SimplifiedCoding
    
    LiveData ViewModel Reference Link:
    https://www.youtube.com/watch?v=1Tn7TuHUl4Y&list=PLlxmoA0rQ-LyVuVR1LFvpR1K8A0HsIBYx&index=6&ab_channel=Smartherd

--- viewmodellivedata/src/main/java/com/mohan/viewmodellivedata/StackResponse.java
+++ viewmodellivedata/src/main/java/com/mohan/viewmodellivedata/StackResponse.java
@@ -8,23 +8,6 @@ public class StackResponse {
     public int quota_max;
     public int quota_remaining;
 
-    public class Item{
-        public Owner owner;
-        public boolean is_accepted;
-        public int score;
-        public int last_activity_date;
-        public int creation_date;
-        public int answer_id;
-        public int question_id;
-        public String content_license;
-        public int last_edit_date;
-    }
-    public class Owner{
-        public int reputation;
-        public int user_id;
-        public String user_type;
-        public String profile_image;
-        public String display_name;
-        public String link;
-    }
+
+
 }
